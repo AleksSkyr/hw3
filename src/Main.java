594583681889df9dd8efbba4805f47b4d463b53b
AleksSkyr <greshnik1499@gmail.com>
2023-04-19 12:54:07 +0300

    HW3 start

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        tasc1();
+    }
+
+        public static void tasc1() {
+            System.out.println("Задача 1");
+
+            byte car = 5;
+            short cat = 200;
+            int appl = 2652;
+            long athlete = 56896;
+            float cofe = 3.2F;
+            double milk = 3.256;
+            System.out.println("Значение переменной с типом данных 'byte' " + car + " равно!");
+            System.out.println("Значение переменной с типом данных 'short' " + cat + " равно!");
+            System.out.println("Значение переменной с типом данных 'int' " + appl + " равно!");
+            System.out.println("Значение переменной с типом данных 'long' " + athlete + " равно!");
+            System.out.println("Значение переменной с типом данных 'float' " + cofe + " равно!");
+            System.out.println("Значение переменной с типом данных 'double' " + milk + " равно!");
+        }
+
+    }
